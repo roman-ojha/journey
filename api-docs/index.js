bbd2b7fad9281e4f48dbb05ee2817f20ab44d38e
Roman-Ojha <razzroman98@gmail.com>
2023-11-13 14:09:06 +0545

    feat: configured Dockerfile & docker-compose for api-docs service

--- api-docs/index.js
+++ api-docs/index.js
@@ -4,16 +4,16 @@ import YAML from "yamljs";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 const app = express();
 const currentFileUrl = import.meta.url;
 const currentFilePath = fileURLToPath(currentFileUrl);
 const __dirname = dirname(currentFilePath);
 
-// Step 3: Load Swagger YAML File
+// Load Swagger YAML File
 const swaggerDocument = YAML.load(path.join(__dirname, "docs.yaml"));
 
-// Step 4: Configure Swagger UI Middleware
+// Configure Swagger UI Middleware
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
