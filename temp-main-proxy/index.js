0dc18aeca7c971d6721e6f7cec9d8dcc851142ea
Roman-Ojha <razzroman98@gmail.com>
2023-11-14 14:04:45 +0545

    refactor: changed gateway -> user-gateway

--- temp-main-proxy/index.js
+++ temp-main-proxy/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import proxy from "express-http-proxy";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/user", proxy(process.env.USER_API_GATEWAY_URL));
 app.use("/api/docs", proxy(process.env.API_DOCS_SERVICE_URL));
--- user-api-gateway/index.ts
+++ user-api-gateway/index.ts
@@ -4,6 +4,10 @@ import proxy from "express-http-proxy";
 const app = express();
 const PORT = process.env.GATEWAY_PORT;
 
+app.get("/", (req, res) => {
+  res.json({ name: "Roman Ojha" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running: http://localhost:${PORT}`);
 });
