646e57caac1e3809142cf81601b6bc749e54d5bc
Roman-Ojha <razzroman98@gmail.com>
2023-11-14 12:15:40 +0545

    feat: init temporary main proxy server with express

--- temp-main-proxy/index.js
+++ temp-main-proxy/index.js
@@ -2,7 +2,10 @@ import express from "express";
 import proxy from "express-http-proxy";
 
 const app = express();
-const PORT = process.env.GATEWAY_PORT;
+const PORT = process.env.PORT;
+
+app.use("/api/user", proxy(process.env.USER_API_GATEWAY_URL));
+app.use("/api/docs", proxy(process.env.API_DOCS_SERVICE_URL));
 
 app.listen(PORT, () => {
   console.log(`Server is running: http://localhost:${PORT}`);
