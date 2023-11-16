1899735fff5933952d9fcbc4f22b9ba3ea91e541
Roman-Ojha <razzroman98@gmail.com>
2023-11-16 15:29:43 +0545

    refactor: changed folder name of service 'api-docs' to 'api-docs-service'

---
+++ api-docs-service/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const PORT = process.env.PORT;
+const app = express();
+const currentFileUrl = import.meta.url;
+const currentFilePath = fileURLToPath(currentFileUrl);
+const __dirname = dirname(currentFilePath);
+
+// Load Swagger YAML File
+const swaggerDocument = YAML.load(path.join(__dirname, "docs.yaml"));
+
+// Configure Swagger UI Middleware
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(PORT, () => {
+  console.log(`Server is running: http://localhost:${PORT}`);
+});
