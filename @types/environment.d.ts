19b333dd3326fac73a0a4af5378496e6d0b0cc13
Roman-Ojha <razzroman98@gmail.com>
2023-11-17 16:12:26 +0545

    feat: docker config on admin-client

--- @types/environment.d.ts
+++ @types/environment.d.ts
@@ -4,10 +4,12 @@ declare namespace NodeJS {
     API_DOCS_SERVICE_PORT: number;
     USER_SERVICE_PORT: number;
     USER_GATEWAY_PORT: number;
+    ADMIN_CLIENT_PORT: number;
     USER_API_GATEWAY_URL: string;
     USER_SERVICE_URL: string;
     API_DOCS_SERVICE_URL: string;
     USER_SERVICE_DATABASE_URL: string;
+    ADMIN_CLIENT_URL: string;
     USER_SERVICE_PUBLIC_SECRET_KEY: string;
     USER_SERVICE_PRIVATE_SECRET_KEY: string;
   }
--- temp-main-proxy/index.js
+++ temp-main-proxy/index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 
 app.use("/api/user", proxy(process.env.USER_API_GATEWAY_URL));
 app.use("/api/docs", proxy(process.env.API_DOCS_SERVICE_URL));
+app.use("/admin", proxy(process.env.ADMIN_CLIENT_URL));
 
 app.listen(PORT, () => {
   console.log(`Server is running: http://localhost:${PORT}`);
