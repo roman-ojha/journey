814aecaffcad30af55c87c895798f2665ef52e5f
Roman-Ojha <razzroman98@gmail.com>
2023-11-17 20:45:50 +0545

    feat(admin-service): create custom admin user & it's manager

--- admin-service/administrator/models.py
+++ admin-service/administrator/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class AdminManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        number = input("Mobile Number: ")
+        extra_fields.setdefault('number', number)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser has to have is_staff being true")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser has to have is_superuser being true")
+
+        return self.create_user(email=email, password=password, **extra_fields)
+
+
+class Admin(AbstractUser):
+    email = models.CharField(max_length=80, unique=True)
+    username = None
+    is_verified = models.BooleanField(default=False)
+    verification_token = models.TextField(default="")
+    number = models.IntegerField(null=False)
+    # first_name
+    # last_name
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['email', 'company_name', '']
+
+    objects = AdminManager()
+
+    def __str__(self) -> str:
+        return self.email
