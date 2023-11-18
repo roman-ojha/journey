ba0858b4351f3c905e067de2651ec7f0ed67fa66
Roman-Ojha <razzroman98@gmail.com>
2023-11-18 15:46:57 +0545

    feat(admin-service): auto generate django model for user service db

--- admin-service/user/models.py
+++ admin-service/user/models.py
@@ -1,3 +1,42 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.TextField(unique=True)
+    number = models.BigIntegerField(unique=True)
+    f_name = models.TextField()
+    l_name = models.TextField()
+    password = models.TextField()
+    photo_url = models.TextField(blank=True, null=True)
+    gender = models.TextField()  # This field type is a guess.
+    is_verified = models.BooleanField()
+    verification_token = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    salt = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'User'
+
+
+class PrismaMigrations(models.Model):
+    id = models.CharField(primary_key=True, max_length=36)
+    checksum = models.CharField(max_length=64)
+    finished_at = models.DateTimeField(blank=True, null=True)
+    migration_name = models.CharField(max_length=255)
+    logs = models.TextField(blank=True, null=True)
+    rolled_back_at = models.DateTimeField(blank=True, null=True)
+    started_at = models.DateTimeField()
+    applied_steps_count = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = '_prisma_migrations'
