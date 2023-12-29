47c10d28c8277460f7c1592b900a3bd498605ffb
Roman-Ojha <razzroman98@gmail.com>
2023-12-29 18:24:31 +0545

    feat(user-client): added hero title section

--- user-client/app/page.tsx
+++ user-client/app/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 import NavBar from "@/components/NavBar";
 import { useState, useEffect } from "react";
+import styles from "@/styles/page/index.module.scss";
 
 type ThemeMode = "light" | "dark";
 
 export default function Home() {
-  return <main>{/* <h1>Home page</h1> */}</main>;
+  return (
+    <main className={styles.home}>
+      <section className={styles.hero_section}>
+        <h1 className={styles.hero_section__title}>
+          Easiest Way to buy ticket Online
+        </h1>
+        <p className={styles.hero_section__info}>
+          We help you to search best Bus, HiAce's vehicles, and buy long
+          distance travel tickets online
+        </p>
+      </section>
+    </main>
+  );
 }
