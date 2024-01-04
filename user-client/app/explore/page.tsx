b4cce63c15ac016ba17c7cdeedca44bdeec90d67
Roman-Ojha <razzroman98@gmail.com>
2024-01-04 17:57:21 +0545

    feat: added HorizontalSearchBox cmp & started explore page

--- user-client/app/explore/page.tsx
+++ user-client/app/explore/page.tsx
@@ -1,12 +1,18 @@
+import HorizontalSearchBox from "@/components/HorizontalSearchBox/HorizontalSearchBox";
 import { APPLICATION_NAME } from "@/data/constants";
 import { Metadata } from "next";
+import styles from "@/styles/page/explore.module.scss";
 
 export const metadata: Metadata = {
   title: `Explore | ${APPLICATION_NAME}`,
 };
 
 const Explore = (): React.JSX.Element => {
-  return <></>;
+  return (
+    <main className={styles.explore_page}>
+      <HorizontalSearchBox />
+    </main>
+  );
 };
 
 export default Explore;
