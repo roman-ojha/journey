c6d55687bb628e26f7a11395340124871b8c6b88
Roman-Ojha <razzroman98@gmail.com>
2024-01-04 23:39:15 +0545

    feat(user-client): created VehicleCard component

---
+++ user-client/components/VehicleCard.tsx
@@ -0,0 +1,73 @@
+"use client";
+import Image from "next/image";
+import styles from "@/styles/components/vehicleCard.module.scss";
+import { Icon } from "@iconify/react";
+
+const VehicleCard = (): React.JSX.Element => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.card_image_container}>
+        <Image
+          src="https://images.unsplash.com/photo-1557223562-6c77ef16210f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="vehicle"
+          width={500}
+          height={500}
+          className={styles.card_image}
+        />
+      </div>
+      <div className={styles.card_info}>
+        <h6 className={styles.card_title}>
+          Matri Vhagya Rekha, AC (Swayambu Yatayat Pvt.Ltd)
+        </h6>
+        <div className={styles.card_rating_review}>
+          <span className={styles.card_rating}>
+            <Icon
+              icon="typcn:star-full-outline"
+              className={styles.card_rating__icon}
+            />
+            <Icon
+              icon="typcn:star-full-outline"
+              className={styles.card_rating__icon}
+            />
+            <Icon
+              icon="typcn:star-full-outline"
+              className={styles.card_rating__icon}
+            />
+            <Icon
+              icon="typcn:star-full-outline"
+              className={styles.card_rating__icon}
+            />
+            <Icon
+              icon="ic:round-star-half"
+              className={styles.card_rating__icon}
+            />
+          </span>
+          <p>20,433</p>
+        </div>
+        <div className={styles.card_departure_at}>
+          <Icon
+            icon="carbon:time-filled"
+            className={styles.card_departure_at__icon}
+          />
+          <p data-card-p="key">Departure AT:</p>
+          <p data-card-p="value">15 DEC, 6:00 AM</p>
+        </div>
+        <div className={styles.card_price}>
+          <Icon icon="raphael:dollar" className={styles.card_price__icon} />
+          <p data-card-p="key">Price</p>
+          <p data-card-p="value">NRS. 1500/-</p>
+        </div>
+        <div className={styles.card_vehicle_type}>
+          <Icon
+            icon="fluent:vehicle-cab-16-filled"
+            className={styles.card_vehicle_type__icon}
+          />
+          <p data-card-p="key">Vehicle Type:</p>
+          <p data-card-p="value">Deluxe Bus</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VehicleCard;
--- user-client/components/pages/explore/VehicleCards.tsx
+++ user-client/components/pages/explore/VehicleCards.tsx
@@ -1,7 +1,16 @@
+import VehicleCard from "@/components/VehicleCard";
 import styles from "@/styles/page/explore/index.module.scss";
 
 const VehicleCards = (): React.JSX.Element => {
-  return <section className={styles.card_container}></section>;
+  return (
+    <section className={styles.card_container}>
+      <VehicleCard />
+      <VehicleCard />
+      <VehicleCard />
+      <VehicleCard />
+      <VehicleCard />
+    </section>
+  );
 };
 
 export default VehicleCards;
