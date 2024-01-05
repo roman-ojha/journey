adb8bff11d89fc196b590577f62d67f4737ae641
Roman-Ojha <razzroman98@gmail.com>
2024-01-05 19:17:50 +0545

    feat(user-client): completed design of VehicleSeats

--- user-client/components/VehicleSeats/DeluxeBusSeats/DeluxeBusSeats.tsx
+++ user-client/components/VehicleSeats/DeluxeBusSeats/DeluxeBusSeats.tsx
@@ -1,27 +1,40 @@
+import AppIcon from "@/components/appIcon/AppIcon";
 import getCssVariable from "@/lib/getCssVariable";
 import styles from "@/styles/page/vehicle/deluxeBusSeats.module.scss";
 
 const DeluxeBusSeats = (): React.JSX.Element => {
   return (
-    <section className={styles.bus_seats_section}>
-      <div className={styles.bus_seats_status}>
+    <section className={styles.vehicle_seats_section}>
+      <div className={styles.vehicle_seats_status}>
         <span
-          className={styles.bus_seats_status__color}
+          className={styles.vehicle_seats_status__color}
           style={{ backgroundColor: getCssVariable("--clr-base-white") }}
         ></span>
         <p>Not Booked</p>
         <span
-          className={styles.bus_seats_status__color}
+          className={styles.vehicle_seats_status__color}
           style={{ backgroundColor: getCssVariable("--clr-base-tertiary") }}
         ></span>
         <p>Booked</p>
         <span
-          className={styles.bus_seats_status__color}
+          className={styles.vehicle_seats_status__color}
           style={{ backgroundColor: getCssVariable("--clr-base-secondary") }}
         ></span>
         <p>Selected</p>
       </div>
-      <div className={styles.bus_seats}></div>
+      <div className={styles.vehicle_seats}>
+        {Array.from({ length: 40 }).map((_, index) => (
+          <span className={styles.vehicle_seat} key={index}>
+            <AppIcon
+              iconName="mdi:seat"
+              use="iconify"
+              className={styles.vehicle_seat__icon}
+            />
+            <b>KA</b>
+            <p>Rs. 1350</p>
+          </span>
+        ))}
+      </div>
     </section>
   );
 };
