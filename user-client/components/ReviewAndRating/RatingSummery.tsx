40306b876270f31368dc31d4af37f7d71046afd8
Roman-Ojha <razzroman98@gmail.com>
2024-01-06 11:48:12 +0545

    feat(user-client): rendering RatingSummery cmp based on it's state

--- user-client/components/ReviewAndRating/RatingSummery.tsx
+++ user-client/components/ReviewAndRating/RatingSummery.tsx
@@ -1,7 +1,17 @@
+"use client";
 import styles from "@/styles/components/reviewAndRating.module.scss";
 import AppIcon from "../appIcon/AppIcon";
+import { useState } from "react";
+import { numberWithCommas } from "@/lib/utils";
+import RatingStar from "../RatingStar";
 
 const RatingSummery = (): React.JSX.Element => {
+  const [rating, setRating] = useState({
+    average: 4.3,
+    total: 20012,
+    ratingPercent: [80, 30, 40, 10, 4], // in percent [5 star, 4 star, 3, 2, 1]
+  });
+
   return (
     <section className={styles.rating_summery_container}>
       <span className={styles.rating_summery_title}>
@@ -9,81 +19,45 @@ const RatingSummery = (): React.JSX.Element => {
       </span>
       <div className={styles.rating_summery}>
         <span className={styles.rating_summery__average}>
-          <h2>4.8</h2>
+          <h2>{rating.average}</h2>
           <span className={styles.rating_summery__average__rating}>
-            <AppIcon
-              iconName="typcn:star-full-outline"
-              use="iconify"
-              className={styles.rating_star_icon}
-            />
-            <AppIcon
-              iconName="typcn:star-full-outline"
-              use="iconify"
-              className={styles.rating_star_icon}
-            />
-            <AppIcon
-              iconName="typcn:star-full-outline"
-              use="iconify"
-              className={styles.rating_star_icon}
-            />
-            <AppIcon
-              iconName="typcn:star-full-outline"
-              use="iconify"
-              className={styles.rating_star_icon}
-            />
-            <AppIcon
-              iconName="typcn:star-full-outline"
-              use="iconify"
+            <RatingStar
+              rating={rating.average}
               className={styles.rating_star_icon}
             />
           </span>
-          <p>Currently Rating</p>
+          <p>{numberWithCommas(rating.total)}</p>
         </span>
         <div className={styles.rating_summery__rating_detail}>
-          <div className={styles.rating_summery__rating_detail__single}>
-            <span
-              className={styles.rating_summery__rating_detail__single__stars}
-            >
-              <AppIcon
-                iconName="typcn:star-full-outline"
-                use="iconify"
-                className={styles.rating_star_icon}
-              />
-              <AppIcon
-                iconName="typcn:star-full-outline"
-                use="iconify"
-                className={styles.rating_star_icon}
-              />
-              <AppIcon
-                iconName="typcn:star-full-outline"
-                use="iconify"
-                className={styles.rating_star_icon}
-              />
-              <AppIcon
-                iconName="typcn:star-full-outline"
-                use="iconify"
-                className={styles.rating_star_icon}
-              />
-              <AppIcon
-                iconName="ic:round-star-half"
-                use="iconify"
-                className={styles.rating_star_icon}
-              />
-            </span>
-            <p>5 Star Rating</p>
-            <span
-              className={
-                styles.rating_summery__rating_detail__single__range_amount
-              }
+          {rating.ratingPercent.map((percent, index) => (
+            <div
+              className={styles.rating_summery__rating_detail__single}
+              key={index}
             >
               <span
+                className={styles.rating_summery__rating_detail__single__stars}
+              >
+                <RatingStar
+                  rating={5 - index}
+                  className={styles.rating_star_icon}
+                  key={index}
+                />
+              </span>
+              <p>{5 - index} Star Rating</p>
+              <span
                 className={
-                  styles.rating_summery__rating_detail__single__range_amount__viewer
+                  styles.rating_summery__rating_detail__single__range_amount
                 }
-                style={{ width: "80%" }}
-              ></span>
-            </span>
-          </div>
+              >
+                <span
+                  className={
+                    styles.rating_summery__rating_detail__single__range_amount__viewer
+                  }
+                  style={{ width: `${percent}%` }}
+                ></span>
+              </span>
+            </div>
+          ))}
         </div>
       </div>
     </section>
