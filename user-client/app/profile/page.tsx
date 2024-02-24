a17c18faa9b04def08639c6fd21c3b2fe91fc99a
Roman-Ojha <razzroman98@gmail.com>
2024-02-25 00:04:20 +0545

    feat(user-client): added profile page

---
+++ user-client/app/profile/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import queryKeys from "@/data/queryKeys";
+import useGetAuthUserQuery from "@/hooks/reactQuery/useGetAuthUserQuery";
+import { useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+
+const Profile = (): React.JSX.Element => {
+  const queryClient = useQueryClient();
+
+  const { data, isError, isSuccess } = useGetAuthUserQuery({
+    retry: false,
+  });
+
+  if (isSuccess) {
+    return (
+      <>
+        <div className="px-12 relative">
+          <div className="p-8 bg-background shadow mt-24 rounded-md">
+            <div className="grid grid-cols-1 md:grid-cols-3">
+              <div className="grid grid-cols-3 text-center order-last md:order-first mt-20 md:mt-0">
+                <div>
+                  <p className="font-bold text-gray-700 text-xl">22</p>
+                  <p className="text-gray-400">Booked Seats</p>
+                </div>
+                {/* <div>
+                  <p className="font-bold text-gray-700 text-xl">10</p>
+                  <p className="text-gray-400">Photos</p>
+                </div> */}
+                <div>
+                  <p className="font-bold text-gray-700 text-xl">89</p>
+                  <p className="text-gray-400">My Reviews</p>
+                </div>
+              </div>
+              <div className="relative">
+                <div className="w-48 h-48 bg-indigo-100 mx-auto rounded-full shadow-2xl absolute inset-x-0 top-0 -mt-24 flex items-center justify-center text-indigo-500">
+                  <Avatar className="w-48 h-48">
+                    <AvatarImage src={data.data.picture} />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                </div>
+              </div>
+              {/* <div className="space-x-8 flex justify-between mt-32 md:mt-0 md:justify-center">
+                <button className="text-white py-2 px-4 uppercase rounded bg-blue-400 hover:bg-blue-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                  Connect
+                </button>
+                <button className="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                  Message
+                </button>
+              </div> */}
+            </div>
+            <div className="mt-20 text-center pb-12">
+              <h1 className="text-4xl font-medium text-gray-50">
+                {data.data.f_name} {data.data.l_name}
+              </h1>
+              <p className="font-light text-gray-100 mt-3">{data.data.email}</p>
+              <p className="font-light text-gray-200 mt-3">
+                {data.data.number}
+              </p>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return <></>;
+  }
+};
+
+export default Profile;
