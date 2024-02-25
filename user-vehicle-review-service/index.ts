45a93daacd1a3488c9e8f62115c462736d0ad6e8
Roman-Ojha <razzroman98@gmail.com>
2024-02-25 19:42:56 +0545

    feat(user-vehicle-review-service): init project

---
+++ user-vehicle-review-service/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { connect as dbConnect, prisma } from "./config/database";
+import router from "./routes";
+import ErrorHandler from "./utils/errorHandler";
+import parseUserCredential from "./middleware/parseUserCredential";
+import { createChannel } from "./config/rabbitMQ";
+
+(async () => {
+  const app = express();
+  const PORT = process.env.MERCHANT_V_AND_T_SERVICE_PORT as string;
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  // Database connection
+  dbConnect();
+
+  // RabbitMQ connection
+  const channel = await createChannel();
+
+  app.use(parseUserCredential);
+  app.use(router(channel));
+  app.use(ErrorHandler);
+
+  // Run server
+  app.listen(PORT, () => {
+    console.log(`Server is running: http://localhost:${PORT}`);
+  });
+})();
