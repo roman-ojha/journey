1128d62b9aca6379417d7a227f66f3abc89f36e4
Roman-Ojha <razzroman98@gmail.com>
2024-02-28 21:29:17 +0545

    fix: now admin service is able to access throw swagger

--- admin-service/core/urls.py
+++ admin-service/core/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('administrator.urls')),
-    path('user', include('user.urls')),
+    # path('admin/', admin.site.urls),
+    path('api/admin/', include('administrator.urls')),
+    path('api/admin/user/', include('user.urls')),
 ]
