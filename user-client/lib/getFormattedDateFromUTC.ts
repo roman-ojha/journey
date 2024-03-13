4043894cab9c651523aa6bcece8fa72f33b4e86f
Roman-Ojha <razzroman98@gmail.com>
2024-03-14 00:12:16 +0545

    fix(user-client): solve not showing actual departuer at time in vehicle cards

--- user-client/lib/getFormattedDateFromUTC.ts
+++ user-client/lib/getFormattedDateFromUTC.ts
@@ -1,9 +1,9 @@
-export default function getFormattedDateFromUTC(UTCDate: Date) {
+export default function getFormattedDateFromUTC(UTCDate: string) {
   return new Intl.DateTimeFormat("en-US", {
     day: "numeric",
     month: "short",
     hour: "numeric",
     minute: "numeric",
     hour12: true,
-  }).format(new Date());
+  }).format(new Date(UTCDate));
 }
